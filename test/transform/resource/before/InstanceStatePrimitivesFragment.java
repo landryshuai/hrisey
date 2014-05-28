9e26575eb6ede2f8ee6e25cea50c3c8d2371ca15
Maciej Górski <maciek.gorski@gmail.com>
2014-05-28 23:46:03 +0200

    added InstanceState test (ignored)

---
+++ test/transform/resource/before/InstanceStatePrimitivesFragment.java
@@ -0,0 +1,24 @@
+// ignore
+
+import hrisey.InstanceState;
+import android.app.Fragment;
+
+public class InstanceStatePrimitivesFragment extends Fragment {
+	
+	@InstanceState
+	private boolean myBool;
+	@InstanceState
+	private byte myByte;
+	@InstanceState
+	private char myChar;
+	@InstanceState
+	private double myDouble;
+	@InstanceState
+	private float myFloat;
+	@InstanceState
+	private int myInt;
+	@InstanceState
+	private long myLong;
+	@InstanceState
+	private short myShort;
+}
